9793773bf1078c522b23e5480c916b4b4a475387
Dominique <dominique.m.mueller@gmail.com>
2016-12-21 10:13:41 +0100

    fix(config): Fixed broken configuration merge
    Interestingly enough, the Object.assign method - originally used to merge
    the custom options into the default options into the configuration object
    itself - only does a shallow merge. Thus, deep objects (which do exist a
    few times) don't get copied properly.
    
    This issue doesn't prevent the configuration completely. However,
    configuration was only possible by either passing no or all options into
    the NotifierConfig constructor.
    
    This issue is now fixed, custom options now get merged properly. A
    procedural flow merges each sub-object explicitely, and other types of
    values are copied / overwritten directly.

--- src/models/notifier-config.model.ts
+++ src/models/notifier-config.model.ts
@@ -114,47 +114,82 @@ export class NotifierConfig implements NotifierOptions {
 	 * @param {NotifierOptions} [customOptions={}] Custom notifier options, optional
 	 */
 	public constructor( customOptions: NotifierOptions = {} ) {
-		const defaultOptions: any = {
-			animations: {
-				enabled: true,
-				hide: {
-					easing: 'ease',
-					offset: 50,
-					preset: 'fade',
-					speed: 300
-				},
-				overlap: 150,
-				shift: {
-					easing: 'ease',
-					speed: 300
-				},
-				show: {
-					easing: 'ease',
-					preset: 'slide',
-					speed: 300
-				}
+
+		// Set default values
+		this.animations = {
+			enabled: true,
+			hide: {
+				easing: 'ease',
+				offset: 50,
+				preset: 'fade',
+				speed: 300
 			},
-			behaviour: {
-				autoHide: 7000,
-				onClick: false,
-				onMouseover: 'pauseAutoHide',
-				showDismissButton: true,
-				stacking: 4
+			overlap: 150,
+			shift: {
+				easing: 'ease',
+				speed: 300
 			},
-			position: {
-				horizontal: {
-					distance: 12,
-					position: 'left'
-				},
-				vertical: {
-					distance: 12,
-					gap: 10,
-					position: 'bottom'
-				}
-			},
-			theme: 'material'
+			show: {
+				easing: 'ease',
+				preset: 'slide',
+				speed: 300
+			}
 		};
-		Object.assign( this, defaultOptions, customOptions ); // Merge all options
+		this.behaviour = {
+			autoHide: 7000,
+			onClick: false,
+			onMouseover: 'pauseAutoHide',
+			showDismissButton: true,
+			stacking: 4
+		};
+		this.position = {
+			horizontal: {
+				distance: 12,
+				position: 'left'
+			},
+			vertical: {
+				distance: 12,
+				gap: 10,
+				position: 'bottom'
+			}
+		};
+		this.theme = 'material'
+
+		// The following merges the custom options into the notifier config, respecting the already set default values
+		// This linear, more explicit and code-sizy workflow is preferred here over a recursive one (because we know the object structure)
+		// Technical sidenote: Objects are merged, other types of values simply overwritten / copied
+		if ( customOptions.theme !== undefined ) {
+			this.theme = customOptions.theme;
+		}
+		if ( customOptions.animations !== undefined ) {
+			if ( customOptions.animations.enabled !== undefined ) {
+				this.animations.enabled = customOptions.animations.enabled;
+			}
+			if ( customOptions.animations.overlap !== undefined ) {
+				this.animations.overlap = customOptions.animations.overlap;
+			}
+			if ( customOptions.animations.hide !== undefined ) {
+				Object.assign( this.animations.hide, customOptions.animations.hide );
+			}
+			if ( customOptions.animations.shift !== undefined ) {
+				Object.assign( this.animations.shift, customOptions.animations.shift );
+			}
+			if ( customOptions.animations.show !== undefined ) {
+				Object.assign( this.animations.show, customOptions.animations.show );
+			}
+		}
+		if ( customOptions.behaviour !== undefined ) {
+			Object.assign( this.behaviour, customOptions.behaviour );
+		}
+		if ( customOptions.position !== undefined ) {
+			if ( customOptions.position.horizontal !== undefined ) {
+				Object.assign( this.position.horizontal, customOptions.position.horizontal );
+			}
+			if ( customOptions.position.vertical !== undefined ) {
+				Object.assign( this.position.vertical, customOptions.position.vertical );
+			}
+		}
+
 	}
 
 }
