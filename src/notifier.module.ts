a501f40cc9791d7a9eaa6acf8cc8ae5972c662e5
Dominique Müller <dominique.m.mueller@gmail.com>
2017-02-05 18:08:39 +0100

    fix(aot): Fixed error occuring when using NotifierModule.forRoot with AoT
    - Extracted NotifierConfig provider into a factory
    - Added a new DI Token for custom options
    
    Closes #5

--- src/notifier.module.ts
+++ src/notifier.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, OpaqueToken } from '@angular/core';
 
 import { NotifierContainerComponent } from './components/notifier-container.component';
 import { NotifierNotificationComponent } from './components/notifier-notification.component';
@@ -7,6 +7,26 @@ import { NotifierConfig, NotifierOptions } from './models/notifier-config.model'
 import { NotifierAnimationService } from './services/notifier-animation.service';
 import { NotifierQueueService } from './services/notifier-queue.service';
 import { NotifierService } from './services/notifier.service';
+
+// tslint:disable variable-name
+/**
+ * Dependency Injection token for custom notifier options
+ */
+export const CustomNotifierOptions: OpaqueToken = new OpaqueToken( 'NotifierOptions' );
+// tslint:enable variable-name
+
+/**
+ * Factory for creating a notifier configuration object
+ *
+ * Sidenote:
+ * This functionality had to be extracted from the NotifierModule.forRoot function, described below. Otherwhise, the Angular AoT compiler
+ * would throw errors. For further details, also see:
+ * - Angular issue #11262 <https://github.com/angular/angular/issues/11262>
+ * - Angular issue #10789 <https://github.com/angular/angular/issues/10789>
+ */
+export function notifierConfigFactory( options: NotifierOptions ): NotifierConfig {
+	return new NotifierConfig( options );
+}
 
 /**
  * Notifier module
@@ -38,8 +58,15 @@ export class NotifierModule {
 			ngModule: NotifierModule,
 			providers: [
 				{
+					provide: CustomNotifierOptions,
+					useValue: options
+				},
+				{
+					deps: [
+						CustomNotifierOptions
+					],
 					provide: NotifierConfig,
-					useValue: new NotifierConfig( options )
+					useFactory: notifierConfigFactory
 				}
 			]
 		};
